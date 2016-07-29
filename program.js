087d4de5b3dc1c783f8c0c44b847894b264848e6
Dmitry Lebedev <p17dmitry@gmail.com>
2016-07-29 18:16:06 +0000

    8. JSON me

--- program.js
+++ program.js
@@ -1,7 +1,17 @@
+var fs = require('fs')
 var express = require('express')
 var app = express()
 
-app.get('/search', function(req, res) {
-    res.send(req.query)
+app.get('/books', function(req, res) {
+    fs.readFile(process.argv[3], function(err, data) {
+        if (err) return res.send(500)
+        try {
+            var books = JSON.parse(data)
+        } catch (e) {
+            res.send(500)
+        }
+        res.json(books)
+    })
 })
+
 app.listen(process.argv[2])
